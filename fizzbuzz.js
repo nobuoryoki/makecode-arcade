c75d53f1f22251890b9b91452cdb55559e6a0e94
Nobuo Ryoki <nobuoryoki@gmail.com>
2021-11-11 22:51:46 +0900

    Add files via upload

---
+++ fizzbuzz.js
@@ -0,0 +1,10 @@
+let number = 10
+if (number % 3 == 0 && number % 5 == 0) {
+    console.log("fizzbuzz")
+} else if (number % 3 == 0) {
+    console.log("fizz")
+} else if (number % 5 == 0) {
+    console.log("buzz")
+} else {
+    console.log(number)
+}
